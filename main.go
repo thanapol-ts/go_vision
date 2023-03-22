0871cdafc2e60176fe76a413b89216cb372eb24b
thanapol sommanapan <thanapols.ts@gmail.com>
2023-03-22 16:06:49 +0700

    update logic

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	vision "cloud.google.com/go/vision/apiv1"
@@ -12,6 +12,11 @@ import (
 )
 
 func main() {
+	var msg struct {
+		NdId   string
+		Name   string
+		Status bool
+	}
 
 	r := gin.Default()
 	r.POST("/check", func(ctx *gin.Context) {
@@ -27,45 +32,49 @@ func main() {
 
 		getFile, err := os.Open("../assets/" + file.Filename)
 		if err != nil {
-			log.Fatalf("Failed to read file: %v", err)
+			fmt.Printf("Failed to read file: %v", err)
 		}
 
 		defer getFile.Close()
 		image, err := vision.NewImageFromReader(getFile)
 		if err != nil {
-			log.Fatalf("Failed to create image: %v", err)
+			fmt.Printf("Failed to create image: %v", err)
 		}
 		labels, err := client.DetectTexts(ctx, image, nil, 10)
 		if err != nil {
-			log.Fatalf("Failed to detect labels: %v", err)
+			fmt.Printf("Failed to detect labels: %v", err)
 		}
 
 		fmt.Println("Labels: ", labels)
 		fmt.Println("============================================================")
 		lines := strings.Split(labels[0].Description, "\n")
-		// var joined = ""
-		// var name = ""
+		var joined = ""
+		var name = ""
 		for index, label := range lines {
 			fmt.Printf("'%d'. '%s'", index, label)
 			fmt.Printf("\n")
-			// if index == 2 {
-			// 	substrings := strings.Split(label, " ")
-			// 	joined = strings.Join(substrings, "")
-			// 	fmt.Println("index == ", index, " ", joined)
-			// }
-			// if index == 4 {
-			// 	st := strings.Split(label, "ชื่อตัวและชื่อสกุล")
-			// 	fmt.Println("index == ", index, " ", st[1])
-			// 	name = st[1]
-			// }
+			if index == 1 {
+				substrings := strings.Split(label, " ")
+				joined = strings.Join(substrings, "")
+				if !CheckID(joined) {
+					msg.Status = false
+					ctx.JSON(http.StatusOK, msg)
+				}
+			}
+
+			if index == 4 {
+				if CheckContains(label) {
+					st := strings.Split(label, "ชื่อตัวและชื่อสกุล")
+					name = st[1]
+				} else {
+					msg.Status = false
+					ctx.JSON(http.StatusOK, msg)
+				}
+			}
 		}
-		var msg struct {
-			NdId   string
-			Name   string
-			Status bool
-		}
-		// msg.Name = name
-		// msg.NdId = joined
+
+		msg.Name = name
+		msg.NdId = joined
 		msg.Status = true
 		fmt.Println("msg", msg)
 		ctx.JSON(http.StatusOK, msg)
@@ -74,4 +83,23 @@ func main() {
 		ctx.String(http.StatusOK, "pass")
 	})
 	r.Run(":4000")
+}
+
+func CheckID(id string) bool {
+	if len(id) != 13 {
+		return false
+	}
+	sum := 0
+	for i := 0; i < 12; i++ {
+		digit, err := strconv.ParseFloat(string(id[i]), 64)
+		if err != nil {
+			return false
+		}
+		sum += int(digit) * (13 - i)
+	}
+	return (11-sum%11)%10 == int(id[12]-'0')
+}
+
+func CheckContains(input string) bool {
+	return strings.Contains(input, "ชื่อสกุล")
 }
