5f3e44986c295c7c2976f7febb5bcca0e7897530
thanapol sommanapan <thanapols.ts@gmail.com>
2023-03-22 11:23:59 +0700

    update

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ IMAGE_NAME="go_vision"
 CONTAINER_NAME="go-vision"
 PORT_NUMBER="4000"
 
+export GOOGLE_APPLICATION_CREDENTIALS="/home/username/service_credentials.json"
+
 docker build -t $IMAGE_NAME .
 
 docker run -e ENV=$ENV -d -p $PORT_NUMBER:$PORT_NUMBER -it $IMAGE_NAME
